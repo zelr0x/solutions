d7d288d0265f8b2c52391f15891832609105a855
zelr0x <38295414+zelr0x@users.noreply.github.com>
2019-05-29 08:48:09 +0400

    Update Pair.java

--- collections/Pair.java
+++ collections/Pair.java
@@ -1,27 +1,34 @@
-// Simple implementation of homogeneous (single-typed) immutable pair.
-// Can be useful in pre java 9 environments where AbstractMap is disallowed 
-// e.g. exercises with limited imports.
+/** 
+ * Simple implementation of homogeneous immutable pair.
+ * Can be useful in pre java 9 environments where AbstractMap is disallowed
+ * e.g. exercises with limited imports.
+ */
 public class Pair<T> {
     private final T first;
     private final T second;
-    Pair(final T first, final T second) {
+    
+    public Pair(final T first, final T second) {
         this.first = first;
         this.second = second;
     }
+    
     public T first() {
         return this.first;
     }
+    
     public T second() {
         return this.second;
     }
+    
     @Override
     public int hashCode() {
         return Objects.hash(first, second);
     }
+    
     @Override
     public boolean equals(final Object other) {
         if (!(other instanceof Pair)) return false;
-
+        
         return this.first.equals(((Pair)other).first()) 
             && this.second.equals(((Pair)other).second());
     }
