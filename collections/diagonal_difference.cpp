172a402aad8b6b633282826833255edc9d4fb7ea
zelr0x <38295414+zelr0x@users.noreply.github.com>
2019-06-09 14:11:28 +0400

    Create diagonal_difference.cpp

---
+++ collections/diagonal_difference.cpp
@@ -0,0 +1,17 @@
+/**
+ * Diagonal difference is the absolute difference of the sums of all elements in both diagonals.
+ * Example:
+ * a b c
+ * d e f
+ * g h i
+ * diff = abs((a + e + i) - (c + e + g))
+ */
+int diagonalDifference(vector<vector<int>> arr) {
+    const auto size = arr.size();
+    auto diff{0};
+    for (auto i = 0; i < size; i++) {
+        diff += arr[i][i];
+        diff -= arr[i][size - i - 1];
+    }
+    return abs(diff);
+}
