b7b1cbb3858edbbebe35d01637d5a976db50c41f
zelr0x <38295414+zelr0x@users.noreply.github.com>
2019-09-27 22:08:35 +0400

    Update next_prime.py

--- numbers/next_prime.py
+++ numbers/next_prime.py
@@ -1,10 +1,9 @@
 def next_prime(x):
     def is_prime(n):
-        if n == 2 or n == 3:
-            return True
-        elif n < 2 or any(n % i == 0 for i in range(2, round(n / 2) + 1)):
-            return False
-        return True
+        return (n == 2 
+                or n == 3 
+                or (n > 2 
+                    and all(n % i != 0 for i in range(2, round(n / 2) + 1))))
 
     while True:
         x += 1
