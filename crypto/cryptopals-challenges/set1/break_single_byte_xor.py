17078664fb87c917c6a8fa4e054ac376d1f4e060
zelr0x <zelr0xdev@gmail.com>
2020-08-17 02:16:55 +0400

    break_single_byte_xor: fixed type hint

--- crypto/cryptopals-challenges/set1/break_single_byte_xor.py
+++ crypto/cryptopals-challenges/set1/break_single_byte_xor.py
@@ -1,11 +1,11 @@
-from typing import Dict, Any, Union
+from typing import Dict
 
 
 def break_single_byte_xor(s: bytes) -> str:
     def xor(byte: int) -> str:
         return ''.join(chr(x) for x in bytearray(s_byte ^ byte for s_byte in s))
 
-    eng_freq: Dict[Union[str, Any], Union[Union[int, float], Any]] = {
+    eng_freq: Dict[str, float] = {
         ' ': 15, 'a': 8.167, 'b': 1.492, 'c': 2.782, 'd': 4.253,
         'e': 12.702, 'f': 2.228, 'g': 2.015, 'h': 6.094, 'i': 6.966,
         'j': 0.153, 'k': 0.772, 'l': 4.025, 'm': 2.406, 'n': 6.749,
