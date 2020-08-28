9c8eb0f8a027ad6f6687a58fa4d1b70123e4a579
zelr0x <zelr0xdev@gmail.com>
2020-08-28 23:47:57 +0400

    fixex_xor.py refactoring

--- crypto/cryptopals-challenges/set1/fixed_xor.py
+++ crypto/cryptopals-challenges/set1/fixed_xor.py
@@ -1,18 +1,18 @@
-def fixed_xor(a: str, b: str) -> str:
-    return (bytearray(ba ^ bb for (ba, bb)
-                      in zip(bytearray.fromhex(a), bytearray.fromhex(b)))
-            ).hex()
+def fixed_xor_hex_str(a: str, b: str) -> str:
+    return fixed_xor(bytearray.fromhex(a), bytearray.fromhex(b)).hex()
+
+
+def fixed_xor(a: bytes, b: bytes) -> bytes:
+    return bytearray(ba ^ bb for (ba, bb) in zip(a, b))
 
 
 def test_fixed_xor():
     test_in_1 = '1c0111001f010100061a024b53535009181c'
     test_in_2 = '686974207468652062756c6c277320657965'
     expected_out = '746865206b696420646f6e277420706c6179'
-    res = fixed_xor(test_in_1, test_in_2)
+    res = fixed_xor_hex_str(test_in_1, test_in_2)
     assert (res == expected_out)
 
 
 if __name__ == '__main__':
-    print('Running tests...')
     test_fixed_xor()
-    print('OK. Passed all tests')
