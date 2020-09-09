36166a49621f0c1f3c8f3cfff8e099b4ff5a4231
zelr0x <38295414+zelr0x@users.noreply.github.com>
2020-09-09 23:23:30 +0400

    Update ObjectUtil.java

--- useful/ObjectUtil.java
+++ useful/ObjectUtil.java
@@ -2,7 +2,7 @@ public final class ObjectUtil {
     public static boolean toBoolOrFalse(Object boolObj) {
         if (boolObj == null) return false;
         if (boolObj instanceof Boolean) return (boolean) boolObj;
-        if (boolObj instanceof Number) return ((Number) boolObj).byteValue() != 0;
+        if (boolObj instanceof Number) return ((Number) boolObj).intValue() != 0;
         return false;
     }
     
