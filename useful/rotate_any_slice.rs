5ff836635209189be23e2752adc3ac6bcc85ff9b
zelr0x <38295414+zelr0x@users.noreply.github.com>
2025-05-03 00:02:54 +0500

    Rename shift_any_slice.rs to rotate_any_slice.rs

--- useful/rotate_any_slice.rs
+++ useful/rotate_any_slice.rs
@@ -1,6 +1,6 @@
 use std::ptr;
 
-pub fn lshift<T>(a: &mut [T], k: usize) {
+pub fn lrot<T>(a: &mut [T], k: usize) {
     let n = a.len();
     let k = k % n;
     if k == 0 {
@@ -15,7 +15,7 @@ pub fn lshift<T>(a: &mut [T], k: usize) {
     }
 }
 
-pub fn rshift<T: std::fmt::Debug>(a: &mut [T], k: usize) {
+pub fn rrot<T: std::fmt::Debug>(a: &mut [T], k: usize) {
     let n = a.len();
     let k = k % n;
     if k == 0 {
@@ -40,35 +40,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn lshift_1_works() {
+    fn lrot_1_works() {
         let mut a = vec![1, 2, 3, 4, 5];
         let exp = vec![2, 3, 4, 5, 1];
-        lshift(&mut a, 1);
+        lrot(&mut a, 1);
         assert_eq!(a, exp);
     }
 
     #[test]
-    fn lshift_2_works() {
+    fn lrot_2_works() {
         let mut a = vec![1, 2, 3, 4, 5];
         let exp = vec![3, 4, 5, 1, 2];
-        lshift(&mut a, 2);
+        lrot(&mut a, 2);
         assert_eq!(a, exp);
     }
 
     #[test]
-    fn rshift_1_works() {
+    fn rrot_1_works() {
         let mut a = vec![1, 2, 3, 4, 5];
         let exp = vec![5, 1, 2, 3, 4];
-        rshift(&mut a, 1);
+        rrot(&mut a, 1);
         assert_eq!(a, exp);
     }
 
 
     #[test]
-    fn rshift_2_works() {
+    fn rrot_2_works() {
         let mut a = vec![1, 2, 3, 4, 5];
         let exp = vec![5, 4, 1, 2, 3];
-        rshift(&mut a, 2);
+        rrot(&mut a, 2);
         assert_eq!(a, exp);
     }
 }
